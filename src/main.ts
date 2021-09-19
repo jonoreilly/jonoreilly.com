e0543ed0845544d60b411fea147b15a66080270a
Jon O'Reilly del Cerro <jon.oreilly@easybell.de>
2021-09-19 15:25:16 +0200

    feat: hardcode google tag manager

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import App from "@/App.vue";
 import "@/registerServiceWorker";
 import router from "@/router";
 import store from "@/store";
-import VueGtag from "vue-gtag";
-import { config } from "@/utils/plugins/gtag";
+// import VueGtag from "vue-gtag";
+// import { config } from "@/utils/plugins/gtag";
 
 createApp(App)
   .use(store)
   .use(router)
-  .use(VueGtag, { config }, router)
+  // .use(VueGtag, { config }, router)
   .mount("#app");
