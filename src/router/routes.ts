f7875a89520c8cc8fa6e64c34bb3db5dea645331
Jon O'Reilly del Cerro <jon.oreilly@easybell.de>
2022-07-21 10:14:22 +0200

    feat: improve SEO

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,7 +4,15 @@ export const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     component: () => import("../views/Home.vue"),
-    meta: { title: "Jon O'Reilly" },
+    meta: {
+      title: "Jon O'Reilly",
+      metaTags: [
+        {
+          name: "description",
+          content: "Hi, I'm Jon O'Reilly. I'm a Front-end engineer.",
+        },
+      ],
+    },
   },
   {
     path: "/projects",
@@ -13,17 +21,44 @@ export const routes: Array<RouteRecordRaw> = [
       {
         path: "webgl",
         component: () => import("../views/projects/WebGL.vue"),
-        meta: { title: "Jon O'Reilly - Projects - WebGL" },
+        meta: {
+          title: "Jon O'Reilly - Projects - WebGL",
+          metaTags: [
+            {
+              name: "description",
+              content:
+                "With the power of WebGL you can view and manipulate 3D objects.",
+            },
+          ],
+        },
       },
       {
         path: "gravity-simulator",
         component: () => import("../views/projects/GravitySimulator.vue"),
-        meta: { title: "Jon O'Reilly - Projects - Gravity simulator" },
+        meta: {
+          title: "Jon O'Reilly - Projects - Gravity simulator",
+          metaTags: [
+            {
+              name: "description",
+              content:
+                "Simulate the Gravitational interactions between multiple planets.",
+            },
+          ],
+        },
       },
       {
         path: "sudoku-solver",
         component: () => import("../views/projects/SudokuSolver.vue"),
-        meta: { title: "Jon O'Reilly - Projects - Sudoku solver" },
+        meta: {
+          title: "Jon O'Reilly - Projects - Sudoku solver",
+          metaTags: [
+            {
+              name: "description",
+              content:
+                "New and improved Sudoku game, with automatic hints and autocomplete functionality.",
+            },
+          ],
+        },
       },
     ],
   },
